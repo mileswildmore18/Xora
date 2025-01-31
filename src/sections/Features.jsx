7d2e9347016d1b3bd240b5c850beef59a5b38bf7
Miles Wildmore <mileswildmore18@gmail.com>
2025-01-31 16:02:32 -0500

    Added the icons to appear on the features section along with adding lines to connect to the icons. Added the paragraph sections for each feature. Next will be to stylize the paragraph section and add descriptions for each topic in the features.

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -6,10 +6,28 @@ const Features = () => {
     <section>
       <Element name="features">
         <div className="container">
-          {/* Add features from the js file  */}
-          <div className="relative flex">
-            {features.map((feature) => (
-              <div key={feature.id}>{feature.title}</div>
+          {/* Make features responsive on different screen sizes*/}
+          <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
+            {/* Add features from the index.js file  */}
+            {features.map(({ id, icon, caption, title, text, button }) => (
+              <div
+                key={id}
+                className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320"
+              >
+                <div className="w-full flex justify-start items-start">
+                  {/*  Make the lines connect to the icons*/}
+                  <div className="-ml-3 mb-12 flex items-center justify-center flex-col">
+                    {/* Add lines going towards the icons*/}
+                    <div className="w-0.5 h-16 bg-s3" />
+                    <img
+                      src={icon}
+                      className="size-28 object-contain"
+                      alt={title}
+                    />
+                  </div>
+                </div>
+                <p className="caption mb-5 max-md:mb-6 max-md:h5">{caption}</p>
+              </div>
             ))}
           </div>
         </div>
