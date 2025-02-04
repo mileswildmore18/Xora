ea2c836b41b77cb1b261960578d87a44fef13d1c
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-04 14:02:39 -0500

    Added a testimonials section. Added the titles. Next will be to add the comments from other people that used the website.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
     </main>
   );
 };
