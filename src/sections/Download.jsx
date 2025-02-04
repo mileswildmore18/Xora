89fc6051f7c2f8289212442f8380282486a4ec42
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-04 15:05:20 -0500

    Added the borders for each download platform as well as adding a picture of the download picture demo for users to see a visual of the Xora software.

--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -1,5 +1,6 @@
 import { Element } from "react-scroll";
 import { links } from "../constants/index.jsx";
+import Marker from "../components/Marker.jsx";
 // Add Download section
 const Download = () => {
   return (
@@ -24,20 +25,59 @@ const Download = () => {
                   alt="xora"
                 />
               </div>
-              <p className="body-1 mb-19 max-w-md">
+              <p className="body-1 mb-10 max-w-md">
                 Try it now for free on iOS, Android, PC, any Web browser. We got
                 you covered
               </p>
+              {/* Add the download links for each platform */}
               <ul className="flex flex-wrap items-center gap-6">
                 {links.map(({ id, url, icon }) => (
                   <li
                     key={id}
                     className="download_tech-link download_tech-link_last-before download_tech-link_last-after"
                   >
-                    {url}
+                    {/* Add the download links  */}
+                    <a
+                      href={url}
+                      /* Add the border around the download icons */
+                      className="size-22 download_tech-icon_before relative flex items-center justify-center rounded-half border-2 border-s3 b-s1 transition-borderColor duration-500"
+                    >
+                      <span className="absolute -top-2 rotate-90">
+                        {/*  Add a marker  */}
+                        <Marker />
+                      </span>
+                      {/*  Add a background image that will appear behind the logo */}
+                      <img
+                        src={"/images/lines.svg"}
+                        alt="lines"
+                        className="absolute size-13/20 object-contain"
+                      />
+                      {/* Add the icon logos for each platform  */}
+                      <span className="download_tech-icon">{icon}</span>
+                    </a>
                   </li>
                 ))}
               </ul>
+            </div>
+            {/* Add a download screen on desktop view*/}
+            <div className="mb-10 max-md:hidden">
+              {/* Add the download screen border */}
+              <div className="download_preview-before download_preview-after rounded-40 relative w-[955px] border-2 border-s5 p-6">
+                {/* Add the download screen inner border*/}
+                <div className="relative rounded-3xl bg-s1 px-6 pb-6 pt-14">
+                  {/* Add the dots */}
+                  <span className="download_preview-dot left-6 bg-p2" />
+                  <span className="download_preview-dot left-11 bg-s3" />
+                  <span className="download_preview-dot left-16 bg-p1/15" />
+                  {/* Add the download screen */}
+                  <img
+                    src="/images/screen.jpg"
+                    width={855}
+                    height={655}
+                    alt="screen"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
