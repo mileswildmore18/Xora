52657d392b46d343a3921a9dc7ea240e61b51b2e
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-04 13:30:48 -0500

    Added the faq icon to add a plus and minus sign with a gorgeous hover effect when user hovers over it. Also made it responsive to the click when user clicsk the icon. Next will be to incorporate the answers to be revealed when user clicks the plus symbol and collapse it when user clicks the minus symbol.

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -2,13 +2,18 @@ import clsx from "clsx";
 import { useState } from "react";
 
 const FaqItem = ({ item, index }) => {
-  const [activeId, setActiveId] = useState();
+  const [activeId, setActiveId] = useState(null);
+  const active = activeId === item.id;
   return (
     //  Add items of FAQ
     <div className="relative z-2 mb-16">
       <div
         className="group relative flex cursor-pointer items-center justify-between gap-10 px-7"
-        onClick={() => {}}
+        /* Add click event for question*/
+        onClick={() => {
+          // Toggle active state of question
+          setActiveId(activeId === item.id ? null : item.id);
+        }}
       >
         <div className="flex-1">
           {/*  Hide the numbers and replace with question */}
@@ -17,13 +22,27 @@ const FaqItem = ({ item, index }) => {
             {index < 10 ? "0" : ""}
             {index}
           </div>
+          {/* Add question and check if it is active  */}
           <div
             className={clsx(
               "h6 text-p4 transition-colors duration-500 max-md:min-h-20 max-md:items-center",
+              active && "max-lg:text-p1",
             )}
           >
             {item.question}
           </div>
+        </div>
+        {/* Add icon for faq  */}
+        <div
+          /*  Add effect for icon when user hovers over it */
+          className={clsx(
+            "faq-icon relative flex size-12 items-center justify-center rounded-full border-2 border-s2 shadow-400 transition-all duration-500 group-hover:border-s4",
+            // Check if question is active and add effect of plus to minus
+            active && "before:bg-p1 after:rotate-0 after:bg-p1",
+          )}
+        >
+          {/* Add icon */}
+          <div className="g4 size-11/12 rounded-full shadow-300" />
         </div>
       </div>
     </div>
